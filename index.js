46e69b72011cab5f7d5a5619d0ff3311ceed4abc
rochaarqueologia@outloo.com <proj.botsaude@gmail.com>
2022-03-12 18:02:58 -0300

    main

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var express = require("express");
 app = express()
 
+
 app.set("view engine", "ejs");
+app.use(express.static('public'));
+
 
 app.get("/", (request, response) => {
     response.render("index");
 });
 
-app.get("/chat", (request, response) => {
+app.get("/farmacovigilancia-imunik", (request, response) => {
     response.render("chat");
 });
 
 app.listen(3306, () => {
-    console.log("Servidor on!");
+    console.log("Servidor online em http://localhost:3306");
 })
